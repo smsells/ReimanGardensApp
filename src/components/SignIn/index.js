8fa539679d2e24954920ec0d34f908071026c441
Bailey Wanders <bwanders@iastate.edu>
2022-10-11 19:40:28 -0500

    improved background image on admin page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,90 +1,80 @@
-import {React, useState}  from 'react';
-import {  Auth } from 'aws-amplify';
-import {useNavigate} from 'react-router-dom';
+import { React, useState } from 'react';
+import { Auth } from 'aws-amplify';
+import { useNavigate } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import "../../css/Sign-In/sign-in.css";
 import { AdminButton } from '../AdminButton/AdminButton';
 
-const SignIn = ({onSignIn}) => {
+const SignIn = ({ onSignIn }) => {
     const navigate = useNavigate();
 
 
-    function navigateHome(){
-       
+    function navigateHome() {
+
         navigate('/');
         //window.location.reload(false);
         onSignIn();
 
     }
-    function pullUser(){
+    function pullUser() {
 
         Auth.currentAuthenticatedUser({
             bypassCache: false
-        }).then(user=> {
+        }).then(user => {
             console.log(user);
             console.log(user.pool);
-            if(user.pool.userPoolId=="us-east-2_tyNlmQmJu"){
-                 console.log("success");
-                 //can redirect in here
+            if (user.pool.userPoolId == "us-east-2_tyNlmQmJu") {
+                console.log("success");
+                //can redirect in here
             }
-        }).catch(err=> console.log(err));
-        
+        }).catch(err => console.log(err));
+
     }
 
-   // function testOnload(){
-   //     console.log("Hey I loaded");
-   // }
+    // function testOnload(){
+    //     console.log("Hey I loaded");
+    // }
 
-   
 
-   
+
+
 
     return (
-    <Authenticator>
-        
-        <div className = 'SignIn' slot= "sign-in" style={{
+        <Authenticator>
 
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            backgroundImage: 'url(https://www.reimangardens.com/wp-content/uploads/2018/01/53-Reiman-Gardens-Entrance-summer.jpg)',
-            backgroundSize: '1800px 1800px'
+            <div className='SignIn' slot="sign-in" style={{
 
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundImage: 'url(https://www.reimangardens.com/wp-content/uploads/2018/01/53-Reiman-Gardens-Entrance-summer.jpg)',
+                backgroundSize: 'cover',
+                maxHeight: 'height',
+                maxWidth: 'width'}}>
 
-        
-        }}>
+                <div style={{
+                    backgroundColor: 'rgba(222, 184, 135, 0.75)', 
+                    height: '100%', 
+                    width: '100%'
+                    }}>
+                        
+                    <AdminButton onClick={navigateHome}>Home</AdminButton>
 
-        <div>
-             <button className='homeButton' onClick={navigateHome}>Home</button>
-        </div>
-        <div>
-            <button className='addButterfly'>Add Butterfly</button>
-            <button className='editButterfly'>Edit Butterfly</button>
-            <button className='adedNotes'>Add/Edit Notes</button>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <AdminButton>View Shipments</AdminButton>
+                        <AdminButton>Add Shipment</AdminButton>
+                        <AdminButton>Import/Export Shipments</AdminButton>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <AdminButton>Add Butterfly</AdminButton>
+                        <AdminButton>Edit Butterfly</AdminButton>
+                        <AdminButton>Add/Edit Notes</AdminButton>
+                    </div>
+                </div>
+            </div>
 
-        <div>
-            <button className='viewShipments'>View Shipments</button>
-            <button className='addShipments'>Add Shipments</button>
-            <button className='imexShipments'>Import/Export Shipments</button>
-
-        </div>
-        <div>
-            <button className='addButterfly'>Add Butterfly</button>
-            <button className='editButterfly'>Edit Butterfly</button>
-            <button className='adedNotes'>Add/Edit Notes</button>
-
-        </div>
-
-            
-            
-
-            
-            <button onClick={pullUser}>Who am I?</button>
-          
-
-        </div>
-     </Authenticator>
+        </Authenticator>
 
     );
 }
@@ -96,7 +86,7 @@ export default SignIn;
 
 /*
 const [user, setUser] = useState('');
-    const [pass, setPass] = useState(''); 
+    const [pass, setPass] = useState('');
      const navigate = useNavigate();
 
     const signIn = async () => {
@@ -116,14 +106,14 @@ const [user, setUser] = useState('');
 <input
                 onChange={e => setUser(e.target.value )}
                 placeholder="Username"
-                
+
               />
               <input
                 onChange={e => setPass(e.target.value )}
                 placeholder="Pass"
-                
+
               />
-            <button  onClick={signIn}>Sign in</button> 
+            <button  onClick={signIn}>Sign in</button>
 */
 //might be easiest to just have a navigation page kinda thing after sign in and the normal page just loads the reiman gardens thing
 //Navigating from the navbar to home currently doesn't auto refresh the page so the signout button switch is broken. 
