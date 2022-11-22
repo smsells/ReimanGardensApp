a4323938b61b723308651de0d8e19f94945b5da8
Sam Sells <samsells@Sams-MacBook-Air.local>
2022-11-22 14:06:54 -0600

    Added map API and organization API call

--- src/components/Parks/index.js
+++ src/components/Parks/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API} from 'aws-amplify';
+import { listOrganizations} from '../../graphql/queries';
 
 
 
 
 const Parks = () =>{
+
+    useEffect(()=>{
+        fetchOrganizations();
+    })
+    async function fetchOrganizations(){
+        const apiData = await API.graphql({
+            query: listOrganizations
+        })
+   
+    const organizationFromAPI = apiData.data.listOrganizations.items;
+   
+    var organizationsState= [];
+    var organizationsCity = [];
+    var i =0;
+    //var organizationsCountry = []
+   organizationFromAPI.forEach(element => {
+        organizationsState[i]= element.locationState;
+        organizationsCity[i]=element.locationCity;
+        //organizationsCountry[i]=element.locationCountry;
+        i++;
+        
+   });
+   console.log(organizationsCity);
+        
+
+    }
     return(
         <div className="Parks">
             <header > Parks Page </header>
